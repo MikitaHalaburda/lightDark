f3ed7199b75512ea08246d0e0064035423986afb
Mikita Halaburda <44066905+MikitaHalaburda@users.noreply.github.com>
2020-01-31 22:43:20 +0300

    light dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import colorsAnyName from "./theme/colors";
 import typography from "./theme/typography";
